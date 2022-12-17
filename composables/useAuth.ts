d2d8bc39e318d73657c68dbd5d99f43a4620b1b2
Jonatan Shoshan <js@lait.dk>
2022-12-17 11:12:52 +0100

    setup auth0

---
+++ composables/useAuth.ts
@@ -0,0 +1,267 @@
+import auth0, { Auth0UserProfile } from "auth0-js";
+import Cookies from "js-cookie";
+
+const userData = ref<Auth0UserProfile | null>(null);
+const auth = ref<auth0.WebAuth | null>(null);
+
+const useAuth = () => {
+	const config = useRuntimeConfig();
+	const AUTH_DOMAIN = config.public.auth0.AUTH_DOMAIN; // DOMAUN from https://auth0.com/ application
+	const AUTH_CLIENTID = config.public.auth0.AUTH_CLIENTID; // CLIENTID from https://auth0.com/ application
+
+	const isLoggedIn = computed(() => !!userData.value);
+
+	if (!auth.value) {
+		if (!AUTH_DOMAIN || AUTH_CLIENTID) {
+			console.error(
+				"runtimeConfig.public.auth0.AUTH_DOMAIN & runtimeConfig.public.auth0.AUTH_DOMAIN are required to be set in nuxt.config, for useAuth to work"
+			);
+		} else {
+			try {
+				auth.value = new auth0.WebAuth({
+					domain: AUTH_DOMAIN,
+					clientID: AUTH_CLIENTID,
+					scope: "email profile openid",
+				});
+			} catch (error) {}
+		}
+	}
+
+	/**
+	 * Logout user
+	 */
+	const logout = () => {
+		if (!auth.value)
+			return console.log("auth0 is not setup correctly - logout failed");
+		Cookies.remove("auth._token.auth0");
+		auth.value.logout({});
+		userData.value = null;
+	};
+
+	/**
+	 * Set userData from token recieved in login functions
+	 */
+	const setUserDataFromToken = (token: string) => {
+		if (!auth.value)
+			return console.log(
+				"auth0 is not setup correctly - setUserDataFromToken failed"
+			);
+
+		auth.value.client.userInfo(token, (error, user) => {
+			if (error) {
+				console.error(error);
+			}
+			if (user) {
+				userData.value = user;
+			}
+		});
+	};
+
+	/**
+	 * Set cookie from token recieved in login functions
+	 */
+	const setAuthCookie = (token: string) => {
+		Cookies.set("auth._token.auth0", "Bearer " + token);
+	};
+
+	/**
+	 * LOGIN WITH GOOGLE
+	 */
+	function loginGoogle({
+		domain,
+		redirectUri,
+	}: {
+		domain: string;
+		redirectUri: string;
+	}) {
+		return new Promise((resolve, reject) => {
+			if (!auth.value)
+				return console.log(
+					"auth0 is not setup correctly - loginGoogle failed"
+				);
+			auth.value.popup.authorize(
+				{
+					owp: true,
+					connection: "google-oauth2",
+					responseType: "token",
+					nonce: "nonce",
+					state: "state",
+					domain,
+					redirectUri,
+				},
+				function (error, authResult) {
+					if (authResult.accessToken) {
+						if (!auth.value)
+							return console.log(
+								"auth0 is not setup correctly - loginGoogle failed"
+							);
+						auth.value.client.userInfo(
+							authResult.accessToken,
+							(error, result) => {
+								if (error) {
+									reject(error);
+								}
+								if (result && authResult.accessToken) {
+									setUserDataFromToken(
+										authResult.accessToken
+									);
+									setAuthCookie(authResult.accessToken);
+									resolve(userData.value);
+								}
+							}
+						);
+					} else {
+						reject(error);
+					}
+				}
+			);
+		});
+	}
+
+	/**
+	 * SEND RESET PASSWORD MAIL
+	 */
+
+	function forgotPassword(forgotPasswordMail: string) {
+		return new Promise((resolve, reject) => {
+			if (!auth.value)
+				return console.log(
+					"auth0 is not setup correctly - forgotPassword failed"
+				);
+			auth.value.changePassword(
+				{
+					email: forgotPasswordMail,
+					connection: "Username-Password-Authentication",
+				},
+				function (error, result) {
+					if (error) {
+						reject(error);
+					}
+					if (result) {
+						resolve(result);
+					}
+				}
+			);
+		});
+	}
+
+	/**
+	 * LOGIN WITH MAIL AND PASSWORD
+	 * N.B. On redirect page -> use processHash function to login user from the return url hash
+	 */
+	function loginLocal(
+		loginEmail: string,
+		loginPassword: string,
+		redirectUri: string
+	) {
+		return new Promise((resolve, reject) => {
+			if (!auth.value)
+				return console.log(
+					"auth0 is not setup correctly - loginLocal failed"
+				);
+			auth.value.login(
+				{
+					email: loginEmail,
+					password: loginPassword,
+					realm: "Username-Password-Authentication",
+					redirectUri,
+					responseType: "token",
+					nonce: "nonce",
+					state: "state",
+				},
+				function (error, result) {
+					if (error) {
+						reject(error);
+					}
+					if (result) {
+						resolve(result);
+					}
+				}
+			);
+		});
+	}
+
+	/**
+	 * REGISETR A USER
+	 */
+	async function register(registerEmail: string, registerPassword: string) {
+		return new Promise((resolve, reject) => {
+			if (!auth.value)
+				return console.log(
+					"auth0 is not setup correctly - register failed"
+				);
+			auth.value.signup(
+				{
+					email: registerEmail,
+					password: registerPassword,
+					connection: "Username-Password-Authentication",
+				},
+				async function (error, result) {
+					if (error) {
+						reject(error);
+					}
+					if (result) {
+						resolve(result);
+					}
+				}
+			);
+		});
+	}
+
+	// GET ACCESS TOKEN FROM HASH AFTER LOGIN REDIRCET
+	/**
+	 * EXAMPLE USE
+	 *
+	 * onMounted(() => {
+	 * 		const hash = route.hash;
+	 * 		if (hash) {
+	 * 			processHash(hash);
+	 * 		}
+	 * }
+	 */
+	const processHash = (hash: string) => {
+		if (!auth.value)
+			return console.log(
+				"auth0 is not setup correctly - processHash failed"
+			);
+		auth.value.parseHash(
+			{ hash, nonce: "nonce", state: "state" },
+			(error, result) => {
+				if (error) {
+					console.error(error);
+				}
+				if (result && result.accessToken) {
+					setAuthCookie(result.accessToken);
+					setUserDataFromToken(result.accessToken);
+				}
+			}
+		);
+	};
+
+	/**
+	 * Set user from cookie
+	 */
+	const setUserFromCookie = () => {
+		let loginToken: string | undefined = Cookies.get("auth._token.auth0");
+		if (loginToken) {
+			loginToken = loginToken.replace("Bearer ", "");
+			setUserDataFromToken(loginToken);
+		}
+	};
+
+	onMounted(() => {
+		setUserFromCookie();
+	});
+
+	return {
+		isLoggedIn,
+		userData,
+		logout,
+		processHash,
+		register,
+		loginLocal,
+		forgotPassword,
+		loginGoogle,
+	};
+};
+export default useAuth;
